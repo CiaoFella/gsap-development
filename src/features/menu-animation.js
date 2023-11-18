ba3bf4c5523f182816599ffe34ef49de999fea53
net11FellaJ <julian.fella@neteleven.de>
2023-11-18 14:49:50 +0100

    added all necessary js files

---
+++ src/features/menu-animation.js
@@ -0,0 +1,18 @@
+const menuClickTrigger = document.querySelector('.menu-wrap.is-menu')
+const allMenuPlanets = document.querySelectorAll('.nav-single-planet')
+allMenuPlanets.forEach((singlePlanet) => {
+  singlePlanet.addEventListener('click', function handlePlanetClick(e) {
+    console.log('clicked')
+    e.stopPropagation()
+    const clickedPlanetDataAttribute = e.target.dataset.ezClickJs
+    const findCorrespondingTab = document.querySelector(
+      `.navigation-tab-step[data-ez-click=${clickedPlanetDataAttribute}]`
+    )
+    if (findCorrespondingTab) {
+      menuClickTrigger.click()
+      setTimeout(() => {
+        findCorrespondingTab.click()
+      }, 500)
+    }
+  })
+})
--- src/main.js
+++ src/main.js
@@ -1,8 +1,5 @@
-import animateTitle from './features/animateTitle'
-import createBadge from './features/createBasge'
-import './styles/style.css'
-
-console.log('Welcome to Vite + JS + Webflow!')
-
-createBadge()
-animateTitle()
+import './features/change-planet'
+import './features/load-page'
+import './features/load-content-page'
+import './features/menu-animation'
+import './features/transition-page'
