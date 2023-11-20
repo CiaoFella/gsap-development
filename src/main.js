97a9024c5e5b7d312ae43015f95ceff9c42b5f7c
net11FellaJ <julian.fella@neteleven.de>
2023-11-20 10:27:42 +0100

    add dynamic imports

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
-import './features/change-planet'
-import './features/load-page'
-import './features/load-content-page'
-import './features/menu-animation'
-import './features/transition-page'
+if (window.location.href.split('/').pop() === '') {
+  import('./features/change-planet')
+  import('./features/load-page')
+  import('./features/load-content-page')
+  import('./features/menu-animation')
+  import('./features/transition-page')
+} else {
+  import('./features/load-content-page')
+  import('./features/transition-page')
+}
