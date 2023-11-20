71490999d78f937a79819c908cb3e4d5a0266ac5
net11FellaJ <julian.fella@neteleven.de>
2023-11-20 15:27:42 +0100

    edit main.js

--- src/main.js
+++ src/main.js
@@ -1,9 +1,5 @@
-if (window.location.href.split('/').pop() === '') {
-  import('./features/load-page').then()
-  import('./features/change-planet').then()
-  import('./features/menu-animation').then()
-  import('./features/transition-page').then()
-} else {
-  import('./features/transition-page').then()
-  import('./features/load-content-page').then()
-}
+import('./features/load-content-page').then()
+import('./features/load-page').then()
+import('./features/change-planet').then()
+import('./features/menu-animation').then()
+import('./features/transition-page').then()
